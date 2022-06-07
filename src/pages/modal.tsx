2514887d2050c0c299a7f261645759d130eb4883
Chris Giannakopoulos <chris@Chriss-MacBook-Pro.local>
2022-06-07 22:44:09 +0100

    feat(implement use react form )

--- src/pages/modal.tsx
+++ src/pages/modal.tsx
@@ -1,32 +1,37 @@
 import {IonButton, IonInput, IonItem, IonLabel, IonToolbar} from '@ionic/react';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
+import {useForm} from "react-hook-form";
 
 const Modal: React.FC<any> = (props) => {
-
-    const [title, setTitle] = useState<string>('')
-
-    const [description, setDescription] = useState<string>('')
+    const {register, handleSubmit} = useForm({
+        mode: "onSubmit",
+        reValidateMode: 'onChange',
+        defaultValues: {name: '', description: ''}
+    });
+    const onSubmit = async (data: any) => {
+        console.log(data)
+    }
 
     useEffect(() => {
         console.log('validation')
-    }, [description, title])
+    }, [onSubmit])
     return (
-        <>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <IonToolbar className={'ion-padding'}>
                 Create a new project
             </IonToolbar>
             <IonItem className="ion-padding">
-                <IonLabel position="floating">Name of the project</IonLabel>
-                <IonInput value={title} onIonChange={e => setTitle(e.detail.value!)}></IonInput>
-            </IonItem>
-            
-            <IonItem className="ion-padding">
-                <IonLabel position="floating">Description of the project</IonLabel>
-                <IonInput value={description} onIonChange={e => setDescription(e.detail.value!)}></IonInput>
+                <IonLabel>Name of the project</IonLabel>
+                <IonInput {...register('name')} ></IonInput>
             </IonItem>
 
-            <IonButton className={'ion-padding'}>Submit</IonButton>
-        </>
+            <IonItem className="ion-padding">
+                <IonLabel>Description of the project</IonLabel>
+                <IonInput {...register('description')} ></IonInput>
+            </IonItem>
+
+            <IonButton type={'submit'} className={'ion-padding'}>Submit</IonButton>
+        </form>
     )
 }
 
